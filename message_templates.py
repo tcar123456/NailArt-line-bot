602a873ea9ccc1b4f092f725b5a4b4852e0ee12d
Craitow <tcar123456@gmail.com>
2025-05-26 01:39:09 +0800

    更新圖文訊息

--- message_templates.py
+++ message_templates.py
@@ -9,12 +9,12 @@ def get_booking_template():
     return ImagemapSendMessage(
         base_url='https://res.cloudinary.com/div4nzzda/image/upload/v1748184390/message_templates2_ugvpah.png',
         alt_text='預約資訊',
-        base_size=BaseSize(height=1040, width=1040),
+        base_size=BaseSize(height=1024, width=1024),
         actions=[
             URIImagemapAction(
                 link_uri='https://jp-tutor.pages.dev/date',
                 area=ImagemapArea(
-                    x=0, y=832, width=1040, height=208
+                    x=0, y=820, width=1024, height=204
                 )
             )
         ]
