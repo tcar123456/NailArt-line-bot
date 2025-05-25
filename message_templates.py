781b09b4bef73a654dc1f3f997077e6ed64d2e41
Craitow <tcar123456@gmail.com>
2025-05-26 01:25:46 +0800

    更新圖文訊息

--- message_templates.py
+++ message_templates.py
@@ -7,7 +7,7 @@ from linebot.models import (
 
 def get_booking_template():
     return ImagemapSendMessage(
-        base_url='https://res.cloudinary.com/div4nzzda/image/upload/w_1040,h_1040,c_fill/v1748184390/message_templates2_ugvpah',
+        base_url='https://res.cloudinary.com/div4nzzda/image/upload/v1748184390/message_templates2_ugvpah.png',
         alt_text='預約資訊',
         base_size=BaseSize(height=1040, width=1040),
         actions=[
