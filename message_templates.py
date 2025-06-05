281a6b8270e7417a5d05234516abe83ef685cb7e
Craitow <tcar123456@gmail.com>
2025-06-05 16:14:07 +0800

    優化圖文選單7

--- message_templates.py
+++ message_templates.py
@@ -7,22 +7,22 @@ from linebot.models import (
 )
 
 def get_booking_template():
-    # 解決版本號碼不同的問題：
-    # 1. 找出兩張圖片中較新的版本號碼
-    # 2. 在 base_url 中指定該版本號碼
-    # 3. 確保兩張圖片都能用相同版本號碼存取
-    
-    # 請檢查您的兩張圖片的版本號碼，使用較新的那個
-    # 例如：如果 message_templates2-1040 是 v1749104975
-    #      如果 message_templates2-700 是 v1749105123
-    #      則使用 v1749105123
+    # 使用 GitHub 作為圖片託管服務
+    # 
+    # 您的圖片網址：
+    # https://raw.githubusercontent.com/tcar123456/images/refs/heads/main/message_templates2-1040.png
+    # https://raw.githubusercontent.com/tcar123456/images/refs/heads/main/message_templates2-700.png
+    #
+    # 範例：如果您的 GitHub 使用者名稱是 'username'，倉庫名稱是 'line-bot'
+    # 則 base_url 應該是：
+    # https://raw.githubusercontent.com/username/line-bot/main/images/message_templates
     
     width = 1040
     height = 1040
 
     return ImagemapSendMessage(
-        # 請將 v1749105123 替換為您實際的較新版本號碼
-        base_url='https://res.cloudinary.com/div4nzzda/image/upload/v1749109784/message_templates2',
+        # 使用正確的 GitHub refs/heads 格式
+        base_url='https://raw.githubusercontent.com/tcar123456/images/refs/heads/main/message_templates2',
         alt_text='預約資訊',
         base_size=BaseSize(height=height, width=width),
         actions=[
